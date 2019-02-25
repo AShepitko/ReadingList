b3019d80dcf48fb23a47c525b5be8a49d956a7b1
Maria Kulkova <mkulkova@distillery.com>
2019-02-25 16:00:24 +0300

    Add book populating tests

--- ReadingList_UnitTests/BookModelTests.swift
+++ ReadingList_UnitTests/BookModelTests.swift
@@ -58,6 +58,8 @@ class MockCoreDataStack {
 
 class BookModelTests: XCTestCase {
 
+    static let validIsbn = "9789604533084"
+
     var mockCoreDataStack = MockCoreDataStack()
 
     // MARK: - Initialize book
@@ -97,31 +99,71 @@ class BookModelTests: XCTestCase {
         XCTAssertEqual(sut.authors, authors)
     }
 
+    func testBookPopulatedWithFetchResult() {
+        let sut = Book(context: mockCoreDataStack.mainContext, readState: .toRead)
+        let fetchResult = FetchResult(fromSearchResult: SearchResult(id: UUID().uuidString, title: "Test", authors: ["test.author"]))
+        fetchResult.isbn13 = ISBN13(BookModelTests.validIsbn)
+        fetchResult.pageCount = 100
+        fetchResult.subjects = ["test", "subject"]
+        fetchResult.languageCode = "en"
+
+        sut.populate(fromFetchResult: fetchResult)
+
+        assertBookInfo(sut, matchingFetchResult: fetchResult)
+    }
+
+    func testBookPopulatedWithSearchResult() {
+        let sut = Book(context: mockCoreDataStack.mainContext, readState: .toRead)
+        let searchResult = SearchResult(id: UUID().uuidString, title: "Test", authors: ["test.author"])
+        searchResult.isbn13 = BookModelTests.validIsbn
+
+        sut.populate(fromSearchResult: searchResult)
+
+        assertBookInfo(sut, matchingSearchResult: searchResult)
+    }
+
+    func testBookPopulatedWithSearchResultSetsCoverImage() {
+        let sut = Book(context: mockCoreDataStack.mainContext, readState: .toRead)
+        let searchResult = SearchResult(id: UUID().uuidString, title: "Test", authors: ["test.author"])
+        let coverImage = Data()
+
+        sut.populate(fromSearchResult: searchResult, withCoverImage: coverImage)
+
+        XCTAssertEqual(sut.coverImage, coverImage)
+    }
+
+    func testBookAuthorsPopulateWithSearchResult() {
+        let sut = Book(context: mockCoreDataStack.mainContext, readState: .toRead)
+        let authors = ["Joan Rowling", "Mark Twen"]
+        let searchResult = SearchResult(id: UUID().uuidString, title: "Test", authors: authors)
+    
+        sut.populate(fromSearchResult: searchResult)
+
+        XCTAssertEqual(sut.authors.map{ $0.displayFirstLast }, authors)
+    }
+
     // MARK: - Fetching books
 
     func testGetBookByGoogleIdReturnsBook() {
         let bookId = "1"
-        _ = insertBookItem(title: "Book 1",
-                           googleBooksId: bookId,
-                           authors: [Author(lastName: "Smith", firstNames: "Jhon")])
+        let book = createTestBookWithGoogleId(bookId, inContext: mockCoreDataStack.mainContext)
 
+        mockCoreDataStack.mainContext.insert(book)
         mockCoreDataStack.saveMainContext()
-        let book = Book.get(fromContext: mockCoreDataStack.mainContext, googleBooksId: bookId, isbn: nil)
+        let fetchedBook = Book.get(fromContext: mockCoreDataStack.mainContext, googleBooksId: bookId, isbn: nil)
 
-        XCTAssertEqual(book?.googleBooksId, bookId)
+        XCTAssertEqual(fetchedBook?.googleBooksId, bookId)
     }
 
     func testGetBookByIsdnReturnsBook() {
-        let bookIsbn: String = "9780451524935"
-        _ = insertBookItem(title: "Book 2",
-                           manualId: "2",
-                           isbn: bookIsbn,
-                           authors: [Author(lastName: "Smith", firstNames: "Jhon")])
+        let bookIsbn = "9780451524935"
+        let book = createTestBookWithIsbn(bookIsbn, inContext: mockCoreDataStack.mainContext)
 
+        mockCoreDataStack.mainContext.insert(book)
         mockCoreDataStack.saveMainContext()
-        let book = Book.get(fromContext: mockCoreDataStack.mainContext, googleBooksId: nil, isbn: bookIsbn)
+        let fetchedBook = Book.get(fromContext: mockCoreDataStack.mainContext, googleBooksId: nil, isbn: bookIsbn)
 
-        XCTAssertEqual(book?.isbn13, isbnNumber(from: bookIsbn))
+        XCTAssertEqual(fetchedBook?.isbn13, isbnNumber(from: bookIsbn))
     }
 
     func testGetBookByIncorrectISDNReturnsNil() {
@@ -141,7 +183,7 @@ class BookModelTests: XCTestCase {
     // MARK: - Book validation
 
     func testEmptyAuthorsThrowsNoAuthorsError() {
-        let book = createTestBookWithGoogleId()
+        let book = createTestBookWithGoogleId(inContext: mockCoreDataStack.mainContext)
 
         book.setAuthors([])
 
@@ -151,7 +193,7 @@ class BookModelTests: XCTestCase {
     }
 
     func testBookWithAuthorsPassesValidation() {
-        let book = createTestBookWithGoogleId()
+        let book = createTestBookWithGoogleId(inContext: mockCoreDataStack.mainContext)
 
         book.setAuthors([Author(lastName: "Smith", firstNames: "Jhon")])
 
@@ -159,7 +201,7 @@ class BookModelTests: XCTestCase {
     }
 
     func testEmptyTitleThrowsValidationError() {
-        let book = createTestBookWithGoogleId()
+        let book = createTestBookWithGoogleId(inContext: mockCoreDataStack.mainContext)
 
         book.title = String()
 
@@ -169,7 +211,7 @@ class BookModelTests: XCTestCase {
     }
 
     func testNotEmptyTitlePassesValidation() {
-        let book = createTestBookWithGoogleId()
+        let book = createTestBookWithGoogleId(inContext: mockCoreDataStack.mainContext)
 
         book.title = "Test"
 
@@ -177,7 +219,7 @@ class BookModelTests: XCTestCase {
     }
 
     func testIncorrectIsbnThrowsError() {
-        let book = createTestBookWithIsbn()
+        let book = createTestBookWithIsbn(inContext: mockCoreDataStack.mainContext)
 
         book.isbn13 = NSNumber(value: 1)
 
@@ -187,7 +229,7 @@ class BookModelTests: XCTestCase {
     }
 
     func testCorrectIsbnPassesValidation() {
-        let book = createTestBookWithIsbn()
+        let book = createTestBookWithIsbn(inContext: mockCoreDataStack.mainContext)
 
         book.isbn13 = NSNumber(value: 9789604533084)
 
@@ -195,7 +237,7 @@ class BookModelTests: XCTestCase {
     }
 
     func testIncorrectLanguageCodeGeneratesError() {
-        let book = createTestBookWithGoogleId()
+        let book = createTestBookWithGoogleId(inContext: mockCoreDataStack.mainContext)
 
         book.languageCode = "pe"
 
@@ -203,9 +245,9 @@ class BookModelTests: XCTestCase {
             XCTAssertEqual(error as NSError, BookValidationError.invalidLanguageCode.NSError())
         }
     }
-    
+
     func testCorrectLanguageCodePassesValidation() {
-        let book = createTestBookWithGoogleId()
+        let book = createTestBookWithGoogleId(inContext: mockCoreDataStack.mainContext)
 
         book.languageCode = "en"
 
@@ -263,6 +305,21 @@ class BookModelTests: XCTestCase {
 
         XCTAssertNotNil(sut.finishedReading)
     }
+    
+    // MARK: - Deletion
+
+    func testBookDeletionDeleteSubjects() {
+        let sut = createTestBookWithGoogleId(inContext: mockCoreDataStack.mainContext)
+        let subjetsNames = ["test", "book"]
+        let subjects = Set(subjetsNames.map { Subject.getOrCreate(inContext: mockCoreDataStack.mainContext, withName: $0) })
+        sut.subjects = subjects
+
+        mockCoreDataStack.mainContext.insert(sut)
+        mockCoreDataStack.saveMainContext()
+        sut.delete()
+
+        subjects.forEach { XCTAssertTrue($0.isDeleted) }
+    }
 
     // MARK: - Tear down
 
@@ -273,42 +330,54 @@ class BookModelTests: XCTestCase {
 
     // MARK: - Helpers
 
-    private func createTestBookWithIsbn() -> Book {
-        let book = Book(context: mockCoreDataStack.mainContext, readState: .toRead)
-        book.title = "Harry Potter"
-        book.isbn13 = NSNumber(value: 9789604533084)
-        book.setAuthors([Author(lastName: "Test", firstNames: "Author")])
-        book.manualBookId = UUID().uuidString
-        book.languageCode = "en"
-
-        return book
-    }
-
-    private func createTestBookWithGoogleId() -> Book {
-        let book = Book(context: mockCoreDataStack.mainContext, readState: .toRead)
-        book.title = "Harry Potter"
-        book.googleBooksId = "1"
-        book.setAuthors([Author(lastName: "Test", firstNames: "Author")])
-        book.languageCode = "en"
-
-        return book
-    }
-
-    private func insertBookItem(title: String,
-                                googleBooksId: String? = nil,
-                                manualId: String? = nil,
-                                isbn: String? = nil,
-                                authors: [Author],
-                                readState: BookReadState = .toRead) -> Book? {
-        let book = NSEntityDescription.insertNewObject(forEntityName: "Book", into: mockCoreDataStack.mainContext) as! Book
-        book.title = title
-        book.googleBooksId = googleBooksId
-        book.manualBookId = manualId
-        book.readState = readState
-        if let currentIsbn = isbn {
-            book.isbn13 = isbnNumber(from: currentIsbn)
+    private func assertBookInfo(_ book: Book, matchingFetchResult fetchResult: FetchResult, file: StaticString = #file, line: UInt = #line) {
+        guard book.googleBooksId == fetchResult.id,
+              book.title == fetchResult.title,
+              book.bookDescription == fetchResult.description,
+              book.languageCode == fetchResult.languageCode,
+              book.publicationDate == fetchResult.publishedDate,
+              book.coverImage == fetchResult.coverImage,
+              book.pageCount?.intValue == fetchResult.pageCount,
+              book.isbn13?.int64Value == fetchResult.isbn13?.int,
+              book.subjects.count == fetchResult.subjects.count,
+              book.authors.count == fetchResult.authors.count else {
+            XCTFail("Book doesn't match fetch result", file: file, line: line)
+            return
         }
-        book.setAuthors(authors)
+    }
+    
+    private func assertBookInfo(_ book: Book, matchingSearchResult searchResult: SearchResult, file: StaticString = #file, line: UInt = #line) {
+        guard book.googleBooksId == searchResult.id,
+              book.title == searchResult.title,
+              book.authors.count == searchResult.authors.count else {
+            XCTFail("Book doesn't match fetch result", file: file, line: line)
+            return
+        }
+        
+        if let searchResultIsbn = searchResult.isbn13, let searchIsbn = isbnNumber(from: searchResultIsbn) {
+            XCTAssertEqual(book.isbn13, searchIsbn, file: file, line: line)
+        }
+    }
+
+    private func createTestBookWithIsbn(_ isbn: String = validIsbn,
+                                        inContext context: NSManagedObjectContext) -> Book {
+        let book = Book(context: context, readState: .toRead)
+        book.manualBookId = UUID().uuidString
+        book.title = "Harry Potter"
+        book.isbn13 = isbnNumber(from: isbn)
+        book.setAuthors([Author(lastName: "Rowling", firstNames: "Joan")])
+        book.languageCode = "en"
+
+        return book
+    }
+
+    private func createTestBookWithGoogleId(_ googleId: String = UUID().uuidString,
+                                            inContext context: NSManagedObjectContext) -> Book {
+        let book = Book(context: context, readState: .toRead)
+        book.googleBooksId = googleId
+        book.title = "Harry Potter"
+        book.setAuthors([Author(lastName: "Rowling", firstNames: "Joan")])
+        book.languageCode = "en"
 
         return book
     }
